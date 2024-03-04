a7114bd20162bd261d086a1e8811702be67f4033
ritik <ritikrajbhagat1@gmail.com>
2024-03-04 17:27:53 +0530

    stage1 login and register done

---
+++ controllers/User/auth.js
@@ -0,0 +1,63 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+
+const User = require("../../models/user");
+
+// 1. POST
+// /api/auth/user/register
+
+const register = asyncHandler(async (req, res) => {
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  const existUser = await User.findOne({ email });
+  if (existUser) {
+    res.status(400);
+    throw new Error("User with same details already exist!");
+  }
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({
+    email,
+    name,
+    password: hashPassword,
+  });
+
+  if (newUser) {
+    res.status(200).json({ message: "registered" });
+  } else {
+    res.status(401);
+    throw new Error("User is not valid");
+  }
+});
+
+// 2. POST
+// /api/auth/user/login
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          id: user.id,
+        },
+      },
+      process.env.SECRET_ACCESS_TOKEN,
+      { expiresIn: "1m" }
+    );
+    res.status(200).json({ token: accessToken, email: email, name: user.name });
+  } else {
+    res.status(401);
+    throw new Error("User is not valid");
+  }
+});
+
+module.exports = { register, login};
--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const cors =  require('cors') ;
 require('dotenv').config() ;
 const connectDB = require('./config/dataBaseConnection')
 
+const auth = require('./routes/User/auth') ;
+
 const app = express() ;
 app.use(express.json()) ;
 app.use(cors()) ;
 connectDB() ;
+
+app.use('/api/auth/user', auth) ;
 
 const port = process.env.PORT ;
 app.listen(port, (req, res)=>{
